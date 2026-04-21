c6133355e852f9354c871ba8ba754206b5ac6eb0
anneisza <rizaanneisza@gmail.com>
2026-04-21 12:16:44 +0700

    menghapus beberapa user

--- src/main/java/repository/UserRepository.java
+++
@@ -1,7 +0,0 @@
-package com.deploy.pertemuan7.repository;
-
-import com.deploy.pertemuan7.model.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository <User, String > {
-}
